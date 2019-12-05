3aa300255282cced1ac79bfbd009556b70d91be3
Alejandro Perez <alex2721ap@gmail.com>
2019-12-05 14:17:26 -0800

    Init Start of Graph

---
+++ Graph/Graph.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Graph
+{
+private:
+	int V;
+	vector<vector<int>>graph;
+	bool* visited;
+
+public:
+	Graph(int v)
+	{
+		this->V = v;
+		visited = new bool[V];
+		for (int i = 0; i < V; i++) { visited[i] = false; }
+		graph.resize(V);
+	}
+
+	void addEdge(int u, int v);
+	void BFS(int x);
+	void DFS(int vertex);
+	bool isBridge(int u, int v);
+};
